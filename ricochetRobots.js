e02e530db5d9b0e098207f894849331139d36796
Angela Vuong <vuong.angela@gmail.com>
2020-03-15 15:02:12 -0400

    placed instance functions in ricochetRobots constructor

--- ricochetRobots.js
+++ ricochetRobots.js
@@ -1,14 +1,10 @@
 class RicochetRobots {
   constructor() {
     this.board = new RicochetGrid(16, 16);
-  }
-  setupRobotBoard() {
     this.board.setWalls(walls);
     this.board.setTargets(targets);
     this.board.initializedRobotPositions();
     this.board.pickNextTarget();
-
-    return this.board;
   }
 
   draw() {}
