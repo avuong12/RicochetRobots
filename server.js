6ec53048724dee7e23fc0e7f4d85c8fffb51db20
Angela Vuong <vuong.angela@gmail.com>
2020-04-17 18:19:19 -0400

    added node server

---
+++ server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 8000; // this can be very useful if you deploy to Heroku!
+app.listen(port, function () {
+  console.log('Knock, knock');
+  console.log("Who's there?");
+  console.log(`Your server, listening on port ${port}`);
+});
