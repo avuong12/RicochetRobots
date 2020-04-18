b8c2b25a5f95ebc588628ca9f33fcac9ff6417a7
Angela Vuong <vuong.angela@gmail.com>
2020-04-18 13:44:42 -0400

    made a client and server directory. npm install sockets

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../client')));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.use(function (err, req, res, next) {
   console.error(err);
